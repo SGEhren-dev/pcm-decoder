11d03bbfd4dfd9b2b3e2a98e8789ff3548d6fbb8
Ehren Grenlund <egrenlund@clintonelectronics.com>
2022-08-10 09:04:57 -0500

    feat(decoder): Added README.md data

--- index.js
+++ index.js
@@ -15,19 +15,7 @@ const isBuffer = require('is-buffer');
 const AV = require('av');
 const { Asset } = require('av');
 
-module.exports = function decode(buffer, opts, cb) {
-    if (opts instanceof Function) {
-        cb = opts;
-        opts = {};
-    }
-
-    if (!opts) {
-        opts = {};
-    }
-
-    if (!cb) {
-        cb = (() => {})
-    }
+module.exports = function decode(buffer) {
 
     if (!isBuffer(buffer)) {
         if (ArrayBuffer.isView(buffer)) {
@@ -42,7 +30,6 @@ module.exports = function decode(buffer, opts, cb) {
 
     if (!type || type !== 'wav') {
         let err = Error('Audio is not in .wav format!');
-        cb(err);
         return Promise.reject(err);
     }
 
@@ -52,10 +39,8 @@ module.exports = function decode(buffer, opts, cb) {
                 channels: audioData.numberOfChannels,
                 sampleRate: audioData.sampleRate
             });
-            cb(null, audBuffer);
             return Promise.resolve(audBuffer);
         }, (error) => {
-            cb(err);
             return Promise.reject(err);
         });
     }
